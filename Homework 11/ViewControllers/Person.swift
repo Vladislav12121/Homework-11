fd21c3c4864299795e7ec6f5eb0296681714c0d6
Vladislav12121 <petr223333@mail.ru>
2023-08-31 22:31:51 +0600

    FirsCommit

---
+++ Homework 11/ViewControllers/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Homework 11
+//
+//  Created by Владислав Клунный on 31.08.2023.
+//
+
+struct Person {
+    let name = DataStore().name
+    let surName = DataStore().surName
+    let email = DataStore().email
+    let phoneNumber = DataStore().phoneNumber
+}
